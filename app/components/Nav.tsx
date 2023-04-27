6c3a5464bb9e4bf8ffdde504223c18b60ab958cb
Soranosuke2001 <sorazora1.4@gmail.com>
2023-04-27 00:33:02 -0700

    installed zustand and created a store

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -13,14 +13,12 @@ export default function Nav({ user }: Session) {
         <h1>Styled</h1>
       </Link>
       <ul className="flex items-center gap-12">
-        {/* If the user does not exist, then render the sign in button */}
         {!user && (
           <li className="bg-teal-600 text-white py-2 px-4 rounded-md">
             <button onClick={() => signIn()}>Sign In</button>
           </li>
         )}
 
-        {/* If the user does exist, then render the profile picture */}
         {user && (
           <li>
             <Image
