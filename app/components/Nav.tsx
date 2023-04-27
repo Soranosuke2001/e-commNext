8e10434bdd44362566158c38037b70f43693a847
Soranosuke2001 <sorazora1.4@gmail.com>
2023-04-27 00:24:05 -0700

    Styled will redirect home and added the metadata to the products page

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { Fragment } from "react";
 import Image from "next/image";
 
 import { Session } from "next-auth";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 export default function Nav({ user }: Session) {
   return (
     <nav className="flex justify-between items-center py-4">
-      <h1>Styled</h1>
+      <Link href={"/"}>
+        <h1>Styled</h1>
+      </Link>
       <ul className="flex items-center gap-12">
         {/* If the user does not exist, then render the sign in button */}
         {!user && (
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -14,6 +14,7 @@ export default async function Product({ searchParams }: SearchParamType) {
       <div className="font-medium text-gray-700">
         <h1 className="text-2xl py-2">{searchParams.name}</h1>
         <p className="py-2">{searchParams.description}</p>
+        <p className="py-2">{searchParams.features}</p>
         <div className="flex gap-2">
           <p className="font-bold text-teal-700">
             {formatPrice(searchParams.unit_amount)}
