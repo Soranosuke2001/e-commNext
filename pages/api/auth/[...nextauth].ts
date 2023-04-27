c79e87e92659124b6a85497303a41610520e5fb4
sora <sorazora1.4@gmail.com>
2023-04-26 20:19:31 -0700

    stripe client is created on new signups

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import Stripe from "stripe";
 
 const prisma = new PrismaClient();
 
@@ -15,7 +16,22 @@ export default NextAuth({
   ],
   events: {
     createUser: async ({ user }) => {
-      
-    }
-  }
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+        apiVersion: "2022-11-15",
+      });
+
+      // Creating a new customer in Stripe
+      if (user.name && user.email) {
+        const customer = await stripe.customers.create({
+          email: user.email,
+          name: user.name,
+        });
+
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { stripeCustomerId: customer.id },
+        })
+      }
+    },
+  },
 });
