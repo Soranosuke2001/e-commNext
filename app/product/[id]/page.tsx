09266265cd3e11d82e856e52a889b5fdbb76fbc4
Soranosuke2001 <sorazora1.4@gmail.com>
2023-04-27 00:20:57 -0700

    added styling to the individual products page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { SearchParamType } from "@/types/SearchParamType";
+import formatPrice from "@/util/priceFormat";
 import Image from "next/image";
 
 export default async function Product({ searchParams }: SearchParamType) {
@@ -10,9 +11,17 @@ export default async function Product({ searchParams }: SearchParamType) {
         width={600}
         height={600}
       />
-      <div>
-        <h1>{searchParams.name}</h1>
-        <p>{searchParams.description}</p>
+      <div className="font-medium text-gray-700">
+        <h1 className="text-2xl py-2">{searchParams.name}</h1>
+        <p className="py-2">{searchParams.description}</p>
+        <div className="flex gap-2">
+          <p className="font-bold text-teal-700">
+            {formatPrice(searchParams.unit_amount)}
+          </p>
+        </div>
+        <button className="my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-700">
+          Add To Cart
+        </button>
       </div>
     </div>
   );
