085b5287f5d95ffb2712be7a562a33434a073b78
sora <sorazora1.4@gmail.com>
2023-04-26 21:20:41 -0700

    readded code in globals.css and styled the navbar

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,18 +1,19 @@
 "use client";
 
+import { Fragment } from "react";
+import Image from "next/image";
+
 import { Session } from "next-auth";
 import { signIn, signOut, useSession } from "next-auth/react";
-import Image from "next/image";
 
 export default function Nav({ user }: Session) {
   return (
     <nav className="flex justify-between items-center py-4">
       <h1>Styled</h1>
-      <ul className='flex items-center gap-12' >
-
+      <ul className="flex items-center gap-12">
         {/* If the user does not exist, then render the sign in button */}
         {!user && (
-          <li className="bg-teal-600 text-white py-2 px-4 rounded-md" >
+          <li className="bg-teal-600 text-white py-2 px-4 rounded-md">
             <button onClick={() => signIn()}>Sign In</button>
           </li>
         )}
