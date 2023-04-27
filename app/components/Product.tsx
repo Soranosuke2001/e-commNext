0cefdd5bacea9892d94f665c549acde04ceb9536
sora <sorazora1.4@gmail.com>
2023-04-26 23:34:20 -0700

    styled the product images

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -6,7 +6,7 @@ import { ProductType } from "@/types/ProductType"
 export default function Product({ name, image, price }: ProductType) {
     return (
         <div>
-            <Image src={image} alt='product image' width={400} height={400} />
+            <Image className='w-full h-80 object-cover' src={image} alt='product image' width={800} height={800} />
             <h1>{name}</h1>
             {formatPrice(price)}
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ const getProducts = async () => {
 export default async function Home() {
   const products = await getProducts();
   return (
-    <main>
+    <main className='grid grid-cols-fluid gap-12'>
       {products.map((product) => (
         <Product key={product.id} {...product} />
       ))}
