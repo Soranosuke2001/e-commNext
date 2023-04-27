2076603971ea4ff9e57656a50875221dcf7c49ba
sora <sorazora1.4@gmail.com>
2023-04-26 23:41:58 -0700

    updating all files in repo

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -2,21 +2,24 @@ import Image from "next/image";
 
 import formatPrice from "@/util/priceFormat";
 import { ProductType } from "@/types/ProductType";
+import Link from "next/link";
 
-export default function Product({ name, image, price }: ProductType) {
+export default function Product({ name, image, price, id }: ProductType) {
   return (
-    <div className="text-gray-700">
-      <Image
-        className="w-full h-80 object-cover rounded-lg"
-        src={image}
-        alt="product image"
-        width={800}
-        height={800}
-      />
-      <div className="font-medium py-2">
-        <h1>{name}</h1>
-        <h2 className="text-sm text-teal-700">{formatPrice(price)}</h2>
+    <Link href={`/${id}`}>
+      <div className="text-gray-700">
+        <Image
+          className="w-full h-80 object-cover rounded-lg"
+          src={image}
+          alt="product image"
+          width={800}
+          height={800}
+        />
+        <div className="font-medium py-2">
+          <h1>{name}</h1>
+          <h2 className="text-sm text-teal-700">{formatPrice(price)}</h2>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
--- types/ProductType.ts
+++ types/ProductType.ts
@@ -1,5 +1,6 @@
 export type ProductType = {
-    name: string,
-    image: string,
-    price: number | null,
-}
+  name: string;
+  image: string;
+  price: number | null;
+  id: string;
+};
