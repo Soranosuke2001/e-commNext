7380ef89c5c17aac788f9155124b8101c21960b1
Soranosuke2001 <sorazora1.4@gmail.com>
2023-04-27 16:20:07 -0700

    added the cart overlay when the cart icon is clicked

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -7,8 +7,13 @@ export default function Cart() {
   console.log(cartStore.isOpen);
 
   return (
-    <div>
-      <h1>Cart Component</h1>
+    <div
+      className="fixed w-full h-screen left-0 top-0 bg-black/25"
+      onClick={() => cartStore.toggleCart()}
+    >
+      <div className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700">
+        <h1>Cart Items</h1>
+      </div>
     </div>
   );
 }
